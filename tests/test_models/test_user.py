c4a41f0159afb0b76c98d1d5355cbb77b14284d0
Esther447 <e.mushimiyi@alustudent.com>
2025-05-07 23:03:21 +0200

    update

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,15 @@
+# tests/test_models/test_user.py
+
+import unittest
+from models.user import User
+
+class TestUser(unittest.TestCase):
+    def test_user_attributes(self):
+        user = User()
+        self.assertIsInstance(user.email, str)
+        self.assertIsInstance(user.password, str)
+        self.assertIsInstance(user.first_name, str)
+        self.assertIsInstance(user.last_name, str)
+
+if __name__ == '__main__':
+    unittest.main()
