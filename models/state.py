6f6940385985b8d5e23964def8c8e924e4dbcb6f
Esther447 <e.mushimiyi@alustudent.com>
2025-05-11 10:00:16 +0200

    update

--- models/state.py
+++ models/state.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-
-"""This file defines the UserModel class
-It inherits from the BaseModel
-"""
-
+"""State module for the AirBnB clone"""
 
 from models.base_model import BaseModel
 
 
-class User(BaseModel):
-    """The User Model"""
-
-    # Attributes
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class State(BaseModel):
+    """State class that inherits from BaseModel"""
+    name = ""
